eb12b344c2e2b82b9a3e630e3a073dc49f1232f3
Mathieu Coulet <coulet.mathieu@gmail.com>
2024-11-19 21:14:44 +0100

    feat(Go auth & config): Go gin using config and Auth

--- server/internal/routers/routers.go
+++ server/internal/routers/routers.go
@@ -1,18 +1,27 @@
 package routers
 
 import (
-	"github.com/ASM-Studios/AREA/internal/controllers"
-	"github.com/ASM-Studios/AREA/internal/middleware"
+	"AREA/internal/config"
+	"AREA/internal/controllers"
+	"AREA/internal/middleware"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(middleware.ErrorHandlerMiddleware())
+	if config.AppConfig.Cors {
+		router.Use(middleware.EnableCors())
+	}
+	if config.AppConfig.Swagger {
+		router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 	public := router.Group("/")
 	{
-		/*public.POST("/register", controllers.Register)
-		public.POST("/login", controllers.Login)*/
+		public.POST("/register", controllers.Register)
+		public.POST("/login", controllers.Login)
 		public.GET("/about.json", controllers.About)
 		public.GET("/ping", controllers.Ping)
 	}
