a94d50b43d58aec108d1c2b7529321c208d26809
Samuel <samuel.bruschet@gmail.com>
2024-12-04 18:54:04 +0100

    fix(spotify): removed useless spotify oauth code

--- client_mobile/lib/features/auth/register.dart
+++ client_mobile/lib/features/auth/register.dart
@@ -18,110 +18,71 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final String callbackUrlScheme = 'my.area.app';
-  String get redirectUrlMobile => '$callbackUrlScheme://callback';
+  String get spotifyRedirectUrlMobile => '$callbackUrlScheme://callback';
 
   final String clientId = dotenv.env["VITE_SPOTIFY_CLIENT_ID"] ?? "";
   final appAuth = FlutterAppAuth();
-  final String clientSecret = dotenv.env["VITE_SPOTIFY_CLIENT_SECRET"] ?? "";
-  final String issuer = "https://accounts.spotify.com/";
-  final Uri authorizationEndpoint =
-      Uri.parse('https://accounts.spotify.com/authorize');
-  final Uri tokenEndpoint = Uri.parse('https://accounts.spotify.com/api/token');
-  final String redirectUrlWeb = "https://localhost:8081/auth/spotify/callback";
-  final Uri redirectUri =
-      Uri.parse('https://localhost:8081/auth/spotify/callback');
-  oauth2.Client? client;
-
-  Future<void> authenticateWithSpotifyOld() async {
-    try {
-      final authorizationTokenRequest = AuthorizationTokenRequest(
-          clientId, redirectUrlMobile,
-          issuer: issuer,
-          clientSecret: clientSecret,
-          scopes: ["openid", "profile", "email", "offline_access"],
-          additionalParameters: {'show_dialog': 'true'});
-
-      final result =
-          await appAuth.authorizeAndExchangeCode(authorizationTokenRequest);
-
-      print("--------------------------------");
-      print("");
-      print("");
-      print("Le code échangé est le suivant : ${result}");
-      print("");
-      print("");
-      print("--------------------------------");
-
-      return;
-    } catch (e) {
-      print('Authentication failed: $e');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: client == null
-            ? Container(
-                padding: const EdgeInsets.all(20),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SimpleText(
-                        "Username",
-                        bold: true,
-                      ),
-                      const AreaFormField(label: "Value"),
-                      const SizedBox(height: 25),
-                      const SimpleText("Password", bold: true),
-                      const AreaFormField(label: "Value"),
-                      const SizedBox(height: 15),
-                      const SimpleText("Confirm password", bold: true),
-                      const AreaFormField(label: "Value"),
-                      const SizedBox(height: 15),
-                      Align(
-                        alignment: Alignment.center,
-                        child: AreaButton(
-                          label: "Register",
-                          onPressed: () {},
-                          color: Colors.black,
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      Align(
-                        alignment: Alignment.center,
-                        child: SignInButton(
-                          onPressed: () {
-                            MicrosoftAuthService.auth(context);
-                          },
-                          label: "Sign in with Microsoft",
-                          image: Image.asset(
-                            "assets/images/microsoft_logo.png",
-                            width: 40,
-                            height: 30,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Align(
-                        alignment: Alignment.center,
-                        child: SmallClickableText(
-                          "I already have an account",
-                          onPressed: () {
-                            context.pop();
-                          },
-                        ),
-                      )
-                    ],
+          child: Container(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SimpleText(
+                "Username",
+                bold: true,
+              ),
+              const AreaFormField(label: "Value"),
+              const SizedBox(height: 25),
+              const SimpleText("Password", bold: true),
+              const AreaFormField(label: "Value"),
+              const SizedBox(height: 15),
+              const SimpleText("Confirm password", bold: true),
+              const AreaFormField(label: "Value"),
+              const SizedBox(height: 15),
+              Align(
+                alignment: Alignment.center,
+                child: AreaButton(
+                  label: "Register",
+                  onPressed: () {},
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Align(
+                alignment: Alignment.center,
+                child: SignInButton(
+                  onPressed: () {
+                    MicrosoftAuthService.auth(context);
+                  },
+                  label: "Sign in with Microsoft",
+                  image: Image.asset(
+                    "assets/images/microsoft_logo.png",
+                    width: 40,
+                    height: 30,
                   ),
                 ),
+              ),
+              const SizedBox(height: 5),
+              Align(
+                alignment: Alignment.center,
+                child: SmallClickableText(
+                  "I already have an account",
+                  onPressed: () {
+                    context.pop();
+                  },
+                ),
               )
-            : Text(
-                'Authenticated! Access token: ${client?.credentials.accessToken}'),
-      ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
