ed0bc8cfaa331d65846ab164ac35c8573621e63a
Samuel <samuel.bruschet@gmail.com>
2024-12-04 17:56:13 +0100

    feat(oauth): getting microsoft access token via oauth

---
+++ client_mobile/lib/features/auth/MicrosoftOAuth.dart
@@ -0,0 +1,39 @@
+// import 'package:aad_oauth/model/config.dart';
+import 'package:aad_oauth/model/config.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:aad_oauth/aad_oauth.dart';
+import '../../main.dart';
+
+class MicrosoftAuthService {
+  static final String clientId = dotenv.env["VITE_MICROSOFT_CLIENT_ID"] ?? "";
+  static final String redirectUri =
+      "msauth://my.area.app/lvGC0B4SWYU8tNPHg%2FbdMjQinZQ%3D";
+  static final String authority = "https://login.microsoftonline.com/common";
+  static final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+  static Future<void> auth(BuildContext context) async {
+    final Config config = Config(
+        tenant: "common",
+        clientId: clientId,
+        scope: "https://graph.microsoft.com/.default",
+        navigatorKey: navigatorKey,
+        redirectUri: redirectUri);
+    config.webUseRedirect = false;
+
+    final AadOAuth oauth = AadOAuth(config);
+    final result = await oauth.login();
+    result.fold(
+      (l) => ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(l.message),
+          backgroundColor: Colors.red,
+        ),
+      ),
+      (r) async {
+        await secureStorage.write(key: 'microsoft_access_token', value: r.accessToken);
+      },
+    );
+  }
+}
