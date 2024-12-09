3dc4885499d8d2ceaf93f2e9c8833205811f3c75
Mathieu Coulet <114529377+Djangss@users.noreply.github.com>
2024-12-09 22:01:44 +0100

    fix(Workflow): Workflow are correctly created with new schema (#136)

--- server/internal/models/event.go
+++ server/internal/models/event.go
@@ -6,12 +6,12 @@ type WorkflowStatus string
 
 type Event struct {
 	gorm.Model
-	Name        string       `json:"name"`
-	Description string       `json:"description"`
-	ServiceID   uint         `gorm:"foreignKey:ServiceID" json:"service_id"`
-	Parameters  []Parameters `json:"parameters"`
-	Type        EventType    `gorm:"type:enum('action', 'reaction');not null" json:"type"`
-        WorkflowEvents []WorkflowEvent `gorm:"constraint:OnDelete:CASCADE;" json:"workflow_events"`
+	Name           string          `json:"name"`
+	Description    string          `json:"description"`
+	ServiceID      uint            `gorm:"foreignKey:ServiceID" json:"service_id"`
+	Parameters     []Parameters    `json:"parameters"`
+	Type           EventType       `gorm:"type:enum('action', 'reaction');not null" json:"type"`
+	WorkflowEvents []WorkflowEvent `gorm:"constraint:OnDelete:CASCADE;" json:"workflow_events"`
 }
 
 const (
