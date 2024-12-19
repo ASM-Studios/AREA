6e3f0dc5a60a5e6e868e50b3b2400acd80bf24f3
Samuel <samuel.bruschet@gmail.com>
2024-12-19 10:27:45 +0100

    fix(regex): turned charset into regex for generation of code verifier

--- client_mobile/lib/tools/utils.dart
+++ client_mobile/lib/tools/utils.dart
@@ -14,23 +14,28 @@ class Utils {
 
     if (password.length < 8)
       error += "Your password must be at least 8 characters.\n";
-  if (!RegExp(r'[a-zA-Z]').hasMatch(password)) {
-    error += "Password must contain at least one letter.\n";
-  }
-  if (!RegExp(r'\d').hasMatch(password)) {
-    error += "Password must contain at least one number.\n";
-  }
-  if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(password)) {
-    error += "Password must contain at least one special character.\n";
-  }
+    if (!RegExp(r'[a-zA-Z]').hasMatch(password)) {
+      error += "Password must contain at least one letter.\n";
+    }
+    if (!RegExp(r'\d').hasMatch(password)) {
+      error += "Password must contain at least one number.\n";
+    }
+    if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(password)) {
+      error += "Password must contain at least one special character.\n";
+    }
     return (error);
   }
 
-    static String generateCodeVerifier() {
-    const String _charset =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
-    return List.generate(
-        128, (i) => _charset[Random.secure().nextInt(_charset.length)]).join();
+  static String generateCodeVerifier() {
+    final random = Random.secure();
+    const String pattern = r'[A-Za-z0-9\-._~]';
+    return List.generate(128, (i) {
+      String char;
+      do {
+        char = String.fromCharCode(random.nextInt(127));
+      } while (!RegExp(pattern).hasMatch(char));
+      return (char);
+    }).join();
   }
 
   static String generateCodeChallenge(String codeVerifier) {
