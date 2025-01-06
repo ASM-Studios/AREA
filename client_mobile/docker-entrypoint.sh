867ef61fb80c9b74e5d28d67e5ff21b28d8bd653
Charles Madjeri <charles@madjeri.com>
2025-01-07 00:38:29 +0100

    feat(client_mobile): add dev server healthcheck
    Signed-off-by: Charles Madjeri <charles@madjeri.com>

---
+++ client_mobile/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+flutter pub get
+
+echo "Building APK..."
+mkdir -p /app/build/app/outputs/flutter-apk
+flutter build apk --release
+mv /app/build/app/outputs/flutter-apk/app-release.apk /app/build/app/outputs/flutter-apk/client.apk
+
+echo "Starting development server..."
+flutter run -d web-server --web-port 8082 --web-hostname 0.0.0.0 --debug 
