690c47c6b2aa014e4019cb23a32a4bf28f3998e9
Samuel <samuel.bruschet@gmail.com>
2025-01-20 14:24:09 +0100

    fix(login): made so that request for login timeout and does not infinite loop if backend is invalid

--- client_mobile/lib/services/login/auth_service.dart
+++ client_mobile/lib/services/login/auth_service.dart
@@ -63,7 +63,7 @@ class AuthService {
       final response = await http.get(
         Uri.parse('$baseUrl/auth/health'),
         headers: {'Authorization': 'Bearer $token'},
-      );
+      ).timeout(Duration(seconds: 5));
       return response.statusCode == 200;
     } catch (e) {
       return false;
